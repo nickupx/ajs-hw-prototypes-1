152cb12d00baf129a76436660768517cd2f00908
nickupx <nick.upsky@gmail.com>
2020-09-24 18:20:20 +0300

    children classes

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -1,58 +1,60 @@
 /* eslint-disable linebreak-style */
-import Character from '../character';
+import {
+  Bowman, Swordsman, Magician, Daemon, Undead, Zombie,
+} from '../character';
 
 test('should create', () => {
-  const result = new Character('Gandalf', 'Magician');
+  const result = new Magician('Gandalf', 'Magician');
   expect(result).toEqual({
     name: 'Gandalf', type: 'Magician', health: 100, level: 1, attack: 10, defence: 40,
   });
 });
 
 test('should create', () => {
-  const result = new Character('Carl', 'Zombie');
+  const result = new Zombie('Carl', 'Zombie');
   expect(result).toEqual({
     name: 'Carl', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10,
   });
 });
 
 test('should create', () => {
-  const result = new Character('Legolas', 'Bowman');
+  const result = new Bowman('Legolas', 'Bowman');
   expect(result).toEqual({
     name: 'Legolas', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
   });
 });
 
 test('should create', () => {
-  const result = new Character('Somebody', 'Daemon');
+  const result = new Daemon('Somebody', 'Daemon');
   expect(result).toEqual({
     name: 'Somebody', health: 100, level: 1, attack: 10, type: 'Daemon', defence: 40,
   });
 });
 
 test('should create', () => {
-  const result = new Character('Somebody', 'Swordsman');
+  const result = new Swordsman('Somebody', 'Swordsman');
   expect(result).toEqual({
     name: 'Somebody', type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10,
   });
 });
 
 test('should create', () => {
-  const result = new Character('Somebody', 'Undead');
+  const result = new Undead('Somebody', 'Undead');
   expect(result).toEqual({
     name: 'Somebody', type: 'Undead', health: 100, level: 1, attack: 25, defence: 25,
   });
 });
 
 test('short name', () => {
-  expect(() => new Character('V', 'Zombie')).toThrow('Incorrect parameters');
+  expect(() => new Zombie('V', 'Zombie')).toThrow('Incorrect parameters');
 });
 
 test('wrong type', () => {
-  expect(() => new Character('Igor', 'Programmer')).toThrow('Incorrect parameters');
+  expect(() => new Zombie('Igor', 'Programmer')).toThrow('Incorrect parameters');
 });
 
 test('should level up', () => {
-  const result = new Character('Somebody', 'Swordsman');
+  const result = new Swordsman('Somebody', 'Swordsman');
   result.levelUp();
   expect(result).toEqual({
     name: 'Somebody', type: 'Swordsman', health: 100, level: 2, attack: 48, defence: 12,
@@ -60,13 +62,13 @@ test('should level up', () => {
 });
 
 test('should not level up', () => {
-  const result = new Character('Somebody', 'Swordsman');
+  const result = new Swordsman('Somebody', 'Swordsman');
   result.health = 0;
   expect(() => result.levelUp()).toThrow('already dead');
 });
 
 test('should damage', () => {
-  const result = new Character('Somebody', 'Swordsman');
+  const result = new Swordsman('Somebody', 'Swordsman');
   result.damage(5);
   expect(result).toEqual({
     name: 'Somebody', type: 'Swordsman', health: 95.5, level: 1, attack: 40, defence: 10,
@@ -74,7 +76,7 @@ test('should damage', () => {
 });
 
 test('should be dead', () => {
-  const result = new Character('Somebody', 'Swordsman');
+  const result = new Swordsman('Somebody', 'Swordsman');
   result.health = 0;
   expect(() => result.damage(10)).toThrow('already dead');
 });
--- src/js/character.js
+++ src/js/character.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable default-case */
 export default class Character {
   constructor(name, type) {
@@ -7,32 +8,6 @@ export default class Character {
       this.type = type;
       this.health = 100;
       this.level = 1;
-      switch (type) {
-        case 'Bowman':
-          this.attack = 25;
-          this.defence = 25;
-          break;
-        case 'Swordsman':
-          this.attack = 40;
-          this.defence = 10;
-          break;
-        case 'Magician':
-          this.attack = 10;
-          this.defence = 40;
-          break;
-        case 'Undead':
-          this.attack = 25;
-          this.defence = 25;
-          break;
-        case 'Zombie':
-          this.attack = 40;
-          this.defence = 10;
-          break;
-        case 'Daemon':
-          this.attack = 10;
-          this.defence = 40;
-          break;
-      }
     } else {
     // выведем универсальную ошибку, лень разбивать логику
       throw new Error('Incorrect parameters');
@@ -56,5 +31,53 @@ export default class Character {
     } else {
       throw new Error('already dead');
     }
+  }
+}
+
+export class Bowman extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+export class Swordsman extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 40;
+    this.defence = 10;
+  }
+}
+
+export class Magician extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+export class Daemon extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+export class Undead extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
+  }
+}
+
+export class Zombie extends Character {
+  constructor(name, type) {
+    super(name, type);
+    this.attack = 40;
+    this.defence = 10;
   }
 }
